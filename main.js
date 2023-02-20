f2137f2117c19cb1f55ed21e25c34ffda2ff4440
Danijel <susakovich@gmail.com>
2023-02-20 22:15:40 +0100

    Fix first input not to be operator

--- main.js
+++ main.js
@@ -24,10 +24,18 @@ const calculator = {
         this.calcAnswer(this.displayText);
         break;
       case "sqrt":
-        this.calcAnswer(Math.pow(this.displayText, 2));
+        if (isNaN(this.displayText)) {
+          this.displayText = "Error";
+        } else {
+          this.calcAnswer(Math.pow(this.displayText, 2));
+        }
         break;
       case "cube":
-        this.calcAnswer(Math.pow(this.displayText, 3));
+        if (isNaN(this.displayText)) {
+          this.displayText = "Error";
+        } else {
+          this.calcAnswer(Math.pow(this.displayText, 3));
+        }
         break;
       case ".":
         if (this.displayText == 0) {
@@ -41,25 +49,32 @@ const calculator = {
         break;
     }
   },
-
   addText(value) {
-    if (this.displayText === "0") {
+    if (this.displayText === "0" && !isNaN(value)) {
       this.displayText = "";
       this.outputText("");
     } else if (this.prevTotal !== null) {
       this.displayText = this.prevTotal;
       this.prevTotal = null;
     }
+
+    // check if the displayText is empty and the entered value is an arithmetic operator
+    if (this.displayText === "" && isNaN(value)) {
+      return;
+    }
+
     // check whether the last char in display AND the entered value are not numbers
     if (isNaN(+value) && isNaN(+this.displayText)) {
       if (isNaN(this.displayText.slice(-1))) {
         return;
       }
     }
+
     // check if value is decimal point and if there is already one present in the display text
     if (value === "." && this.displayText.includes(".")) {
       return;
     }
+
     this.displayText += value;
     this.outputText(this.displayText);
   },
